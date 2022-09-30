482b5c68c0da4501be91e57d34ecdc90daeffde7
wspenc <105823133+wspenc@users.noreply.github.com>
2022-09-30 15:27:44 -0600

    Update sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -7,8 +7,8 @@
 // Planning
 // Analysis of Requirements
 // Design
-// Testing & Integration
 // Implementation
+// Testing & Integration
 // Maintain
 
 
@@ -19,13 +19,12 @@
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
-
 const analysis =  "This is the second step, which is gathering information from a business stand point. From there utilizing the information to show what you want the code to run. ";
 
 const design = "This is the third step, which is when you figure out what you want the page to look like. From fonts, images, how you want the page to interact on certain commands, etc.";
 
-const testing = "This is the fourth step, where after all the pieces have been brought together to test the functionallity and to see if there are any fixes that need to made.";
+const implementation = "This is the fifth step, carrying out the desgins.";
 
-const implementation = "This is the fifth step, where the code has been launched and is running properly.";
+const testing = "This is the fourth step, where after all the pieces have been brought together to test the functionallity and to see if there are any fixes that need to made. Should be functional";
 
-const maintain = "This is the sixth step, when you are continually adding updates, fixes, and ensuring it runs properly.";
+const maintain = "This is the sixth step, when you are adding updates, fixes, and ensuring it runs properly.";
